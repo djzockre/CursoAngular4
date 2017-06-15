41c154349075a710ea638dd5aa81ae1cc09f8d80
djzockre <ferdinand_sockre@comunidad.unam.mx>
2017-06-14 22:47:48 -0500

    Avance del curso con directivas

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -9,7 +9,9 @@ export class EmpleadoComponent{
   public titulo = 'Componente empleados';
   public empleado:Empleado;
   public trabajadores:Array<Empleado>;
-  public trabajador_Externo:boolean;
+  public trabajador_externo:boolean;
+  public color:string;
+  public color_seleccionado:string;
 
   constructor(){
     this.empleado = new Empleado('Fernando Ruiz', 45,'Fullstack', true);
@@ -20,7 +22,9 @@ export class EmpleadoComponent{
 
     ];
 
-    this.trabajador_Externo = false;
+    this.trabajador_externo = false;
+    this.color= 'blue';
+    this.color_seleccionado = '#ccc';
 
   }
 
@@ -28,6 +32,11 @@ export class EmpleadoComponent{
   //Peticiones AJAX
    console.log(this.empleado);
    console.log(this.trabajadores);
+
+ }
+
+ cambiarExterno(valor){
+   this.trabajador_externo = valor;
 
  }
 
